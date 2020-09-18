514c9bcb6bdbb89518ae593b577682fd7027f236
JeanCDev <jeangames15@gmail.com>
2020-09-18 16:10:37 -0300

    Adicionando Rotas no React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,8 +8,8 @@ import {
   Play_400Regular,
   Play_700Bold
 } from '@expo-google-fonts/play';
-import Header from './src/components/Header';
-import Home from './src/pages/Home';
+
+import Routes from './src/Routes';
 
 export default function App() {
 
@@ -25,8 +25,7 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <Header />
-        <Home />
+        <Routes />
         <StatusBar style="light" />
       </View>
     );
@@ -36,6 +35,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0B1F34',
   }
 });
